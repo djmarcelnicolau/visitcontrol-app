1cc3433b0193720ac0694de4491e499f7d9511ce
djmarcelnicolau <djmarcelnicolau@gmail.com>
2025-05-12 15:27:37 -0300

    Create main.dart

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'VisitControl',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
